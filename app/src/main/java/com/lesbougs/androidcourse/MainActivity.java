dbcad96086a2c79592cfe6361a2cb3f7ba555bc9
MrFlavZz <flav621@gmail.com>
2020-09-18 18:41:52 +0200

    empty project

---
+++ app/src/main/java/com/lesbougs/androidcourse/MainActivity.java
@@ -0,0 +1,14 @@
+package com.lesbougs.androidcourse;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    a
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
